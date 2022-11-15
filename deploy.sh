430a306c135ca8d9e0fc1a1df3e9634daddb0d32
Franklin P. Ramos <franky88@users.noreply.github.com>
2022-11-15 15:36:02 +0800

    updates

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:franky88/mypages.git main:gh-pages
+git push -f git@github.com:franky88/mypages.git master:gh-pages
 cd -
