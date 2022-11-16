a28736191de5cbc3bd0ce665900126d694453047
Franklin P. Ramos <franky88@users.noreply.github.com>
2022-11-16 08:46:07 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mypages/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
